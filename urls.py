91aa1e2d3b524bbcc2d3d9d121139ddd6b6fc112
liuxiwang <52487299+liu-xiwang@users.noreply.github.com>
2020-05-22 18:33:55 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^account/', include('blueapps.account.urls')),
     # 如果你习惯使用 Django 模板，请在 home_application 里开发你的应用，
     # 这里的 home_application 可以改成你想要的名字
-    url(r'^', include('home_application.urls')),
+    # url(r'^', include('home_application.urls')),
     url(r'^exam/', include('exam.urls')),
     # 如果你习惯使用 mako 模板，请在 mako_application 里开发你的应用，
     # 这里的 mako_application 可以改成你想要的名字
