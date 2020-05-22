a90e2901882b563a420b36686844b1e233706b05
liuxiwang <52487299+liu-xiwang@users.noreply.github.com>
2020-05-22 17:14:55 +0800

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -27,15 +27,15 @@ def get_env_or_raise(key):
 
 # 这些变量将由平台通过环境变量提供给应用，本地开发时需手动配置，详见：http://docs.open.oa.com/topics/company_tencent/python_framework_usage#29-配置环境变量
 # 应用 ID
-APP_CODE = 'lanjinexam'
+APP_CODE = ''
 # 应用用于调用云 API 的 Secret
-SECRET_KEY = '723b235f-8fff-4cb0-89b4-b8ca7e790258'
+SECRET_KEY = ''
 
 
 # SaaS运行版本，如非必要请勿修改
 RUN_VER = 'open'
 # 蓝鲸SaaS平台URL，例如 http://paas.bking.com
-BK_URL = "http://paas.bk-ty.com:80"
+BK_URL = ""
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(
