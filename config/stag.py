11b56fdd64c7cc7297e02c0e674f8160185b15dc
zhengjia1989 <44701086+zhengjia1989@users.noreply.github.com>
2020-05-28 19:05:52 +0800

    Add files via upload

---
+++ config/stag.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from config import RUN_VER
+if RUN_VER == 'open':
+    from blueapps.patch.settings_open_saas import *  # noqa
+else:
+    from blueapps.patch.settings_paas_services import  * # noqa
+
+# 预发布环境
+RUN_MODE = 'STAGING'
+
+# 正式环境的日志级别可以在这里配置
+# V2
+# import logging
+# logging.getLogger('root').setLevel('INFO')
+# V3
+# import logging
+# logging.getLogger('app').setLevel('INFO')
+
+
+# 预发布环境数据库可以在这里配置
+
+DATABASES.update(
+    {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'wordpress',  #  数据库名
+            'USER': 'wordpress',  #  用户
+            'PASSWORD': 'Bgx123.com',  # 数据库密码
+            'HOST': '192.168.50.118', # 数据库 IP
+            'PORT': '3306',
+        },
+    }
+)
+
