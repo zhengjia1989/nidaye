c781becb14cf17f043db932cd5e075486f3d8584
zhengjia1989 <44701086+zhengjia1989@users.noreply.github.com>
2020-05-28 18:59:43 +0800

    Update and rename config/__init__.py to 配置/__init__.py

--- 配置/__init__.py
+++ 配置/__init__.py
@@ -27,9 +27,9 @@ def get_env_or_raise(key):
 
 # 这些变量将由平台通过环境变量提供给应用，本地开发时需手动配置，详见：http://docs.open.oa.com/topics/company_tencent/python_framework_usage#29-配置环境变量
 # 应用 ID
-APP_CODE = 'wordpres'
+APP_CODE = 'wordpress'
 # 应用用于调用云 API 的 Secret
-SECRET_KEY = 'da8d9b51-9245-4434-92e0-66ca77cd5cfa'
+SECRET_KEY = '1bb06165-6a27-4303-9bec-177d9e083b98'
 
 
 # SaaS运行版本，如非必要请勿修改
